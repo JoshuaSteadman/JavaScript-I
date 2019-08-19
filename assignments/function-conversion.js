a066b002a54a58dccb21719595d1402856455f8d
Joshua Steadman <sintal1ty@gmail.com>
2019-08-19 17:16:53 -0400

    functions.js complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,23 +4,36 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+(myFunction => {
+    console.log("Function was invoked!");
+})();
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+anotherFunction = (param) => {
+    return param;
+}
+anotherFunction("Exmaple");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+add = (a, b) => {
+   return a + b;
+}
+console.log(add(1,2));
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
+subtract = (a, b) => {
+    return a-b;
+}
+console.log(subtract(1,2));
 // Stretch
 
 // exampleArray = [1,2,3,4];
