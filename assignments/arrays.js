8c01a3bc3b326b9cd677ebfd4b5e48be3fc43cea
Joshua Steadman <sintal1ty@gmail.com>
2019-08-19 19:45:03 -0400

    Fix

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -124,6 +124,7 @@ let BMWAndAudi = [];
 for (let i = 0; i < inventory.length; i++) {
   if(inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi") {
   BMWAndAudi.push(inventory[i]);
+  JSON.stringify(BMWAndAudi);
   }
 }
-console.log(JSON.stringify(BMWAndAudi);
+console.log(BMWAndAudi);
